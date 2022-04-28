72fe91e73692c24f27c079b61038ae19d65a227c
Pedro Bueno <99371991+PBuenoc@users.noreply.github.com>
2022-04-28 01:24:34 -0300

    Add studies about polymorphism

---
+++ bytebank-herdado-conta/src/ContaCorrente.java
@@ -0,0 +1,24 @@
+
+public class ContaCorrente extends Conta implements Tributavel {
+	
+	public ContaCorrente(int agencia, int numero) {
+		super(agencia, numero);
+	}
+	
+	@Override
+	public boolean saca(double valor) {
+		return super.saca(valor + 0.20);
+	}
+
+	@Override
+	public void deposita(double valor) {
+		super.saldo += valor;
+		
+	}
+
+	@Override
+	public double getValorImposto() {
+		return super.saldo * 0.01;
+	}
+	
+}
